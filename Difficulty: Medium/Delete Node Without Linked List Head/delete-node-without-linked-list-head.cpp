5a104283e001d068879cf3a0173fb6a29d1e400a
ars231106 <aryansurani32@gmail.com>
2026-08-28 01:24:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Delete Node Without Linked List Head/delete-node-without-linked-list-head.cpp
@@ -0,0 +1,21 @@
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    void deleteNode(Node* x) {
+        Node* temp = x -> next;
+        x -> data = temp -> data;
+        x -> next = temp -> next;
+        delete temp;
+    }
+};
