c059b8a98dd5b20ca734b6a9828bb21247f12182
ars231106 <aryansurani32@gmail.com>
2026-08-29 09:55:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove Cycle in Linked List/remove-cycle-in-linked-list.cpp
@@ -0,0 +1,36 @@
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+*/
+
+
+class Solution {
+public:
+    void removeLoop(Node* head) {
+        Node* p = NULL;
+        Node* temp = head;
+
+        unordered_map<Node*, bool> visited;
+
+        while(temp != NULL){
+
+            if(visited[temp]){
+                p->next = NULL;
+                return;
+            }
+
+            visited[temp] = true;
+
+            p = temp;
+            temp = temp->next;
+        }
+    }
+};
