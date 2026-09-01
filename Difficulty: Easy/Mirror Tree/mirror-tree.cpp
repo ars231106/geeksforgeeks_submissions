cee17fc18efb617ae0024cd500a7758283c2e4d0
ars231106 <aryansurani32@gmail.com>
2026-09-01 08:57:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Mirror Tree/mirror-tree.cpp
@@ -0,0 +1,30 @@
+/* Structure of Binary Tree Node
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    void mirror(Node* root) {
+        if(root == NULL){
+            return;
+        }
+        
+        Node* temp = root -> left;
+        root -> left = root -> right;
+        root -> right = temp;
+        
+        mirror(root -> left);
+        mirror(root ->right);
+        
+        return;
+        
+    }
+};
