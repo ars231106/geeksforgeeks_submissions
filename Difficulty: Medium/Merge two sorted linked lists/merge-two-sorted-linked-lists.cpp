2112538b581933fc7176655a88cfe2f410310600
ars231106 <aryansurani32@gmail.com>
2026-09-06 21:34:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -16,7 +16,7 @@ class Solution {
     Node* sortedMerge(Node* head1, Node* head2) {
             Node* head = new Node(0);
             Node* tail = head;
-            
+
             while(head1 != NULL && head2 != NULL){
                 if(head1 -> data <= head2 -> data){
                     tail -> next = head1;
@@ -24,7 +24,7 @@ class Solution {
                     tail = tail -> next;
                     tail -> next = NULL;
                 }
-                
+
                 else{
                     tail -> next = head2;
                     head2 = head2 -> next;
@@ -32,20 +32,20 @@ class Solution {
                     tail -> next = NULL;
                 }
             }
-            
+
             if(head1){
                 tail -> next = head1;
             }
-            
+
             if(head2){
                 tail -> next = head2;
             }
-            
+
             tail = head;
             head = head -> next;
             delete tail;
-            
+
             return head;    
-        
+
     }
 };
